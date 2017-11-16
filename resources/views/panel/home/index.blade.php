4470b6707c4980c57bf929bd22e526dbb0f612ca
Fabiano Falcão <fabiano.falcao@ifmg.edu.br>
2017-11-15 22:18:18 -0200

    Inicio Integração Layout SD Admin ao Laravel 55

---
+++ resources/views/panel/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('panel.layout.template')
+
+@section('content')
+
+<h1>Home Page Dashborad</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$this->get('panel', 'Panel\PanelController@index');
